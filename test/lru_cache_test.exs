1dee4362e2b3b22920218f78d7f722614438b8f5
Nikola Ostojic <nikolaostoji@gmail.com>
2021-02-14 02:08:31 -0500

    - added docs - created unit tests - added basic calls

--- test/lru_cache_test.exs
+++ test/lru_cache_test.exs
@@ -2,7 +2,30 @@ defmodule LruCacheTest do
   use ExUnit.Case
   doctest LruCache
 
-  test "greets the world" do
-    assert LruCache.hello() == :world
+  setup do
+    {:ok,server_pid} = LruCache.create()
+    {:ok,server: server_pid}
+  end
+
+  test "tests put() returns true when adding element" do
+    assert LruCache.put("abc", "123") == true
+  end
+
+  test "tests get() returns ok if key is in cache" do
+    LruCache.put("abc", "123")
+    assert LruCache.get("abc") == "123"
+  end
+
+  test "tests get() returns nil if key DNE in cache" do
+    assert LruCache.get("abc") == nil
+  end
+
+  test "tests delete() removes element" do
+    LruCache.put("abc", "123")
+    assert LruCache.get("abc") == "123"
+
+    LruCache.delete("abc")
+    assert LruCache.get("abc") == nil
   end
 end
+
